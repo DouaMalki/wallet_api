4b8a9377a368778bf6a37f3f70ead10053da35ee
DouaMalki <166130291+DouaMalki@users.noreply.github.com>
2025-12-28 17:12:33 +0200

    update

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -1,24 +1,14 @@
 import { sql } from "../config/db.js";
 
-export async function syncUserAfterFirebaseAuth(req, res) {
+export async function signUpUser(req, res) {
   try {
     const { firebase_uid, name, email } = req.body;
+
     if (!firebase_uid) {
-      return res.status(400).json({ message: "Missing required field" });
+      return res.status(400).json({ message: "Missing required fields" });
     }
 
-    // Update the last login date if the user exists
-    const updated = await sql`
-      UPDATE users
-      SET last_login = CURRENT_TIMESTAMP
-      WHERE firebase_uid = ${firebase_uid}
-      RETURNING user_id, name, email, points, level
-    `;
-    if (updated.length > 0) {
-      return res.json(updated[0]);
-    }
-
-    // Insert the user if it is not exist
+    // Insert new user
     const inserted = await sql`
       INSERT INTO users (
         firebase_uid,
@@ -39,6 +29,32 @@ export async function syncUserAfterFirebaseAuth(req, res) {
     res.json(inserted[0]);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: "failed" });
+    res.status(500).json({ message: "Signup failed" });
+  }
+}
+
+export async function loginUser(req, res) {
+  try {
+    const { firebase_uid } = req.body;
+
+    if (!firebase_uid) {
+      return res.status(400).json({ message: "Missing firebase UID" });
+    }
+
+    // Update last_login date
+    const updated = await sql`
+      UPDATE users
+      SET last_login = CURRENT_TIMESTAMP
+      WHERE firebase_uid = ${firebase_uid}
+      RETURNING user_id, name, email, points, level
+    `;
+
+    if (updated.length === 0) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(updated[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Login failed" });
   }
 }
--- routers/authenticationRouter.js
+++ routers/authenticationRouter.js
@@ -1,12 +1,14 @@
 import express from "express";
 import {
-  syncUserAfterFirebaseAuth
+  signUpUser,
+  loginUser
 } from "../controllers/authenticationController.js";
 
 
 const router = express.Router();
 
 // sync the user after authentication (login or sign up)
-router.post("/authentication", syncUserAfterFirebaseAuth);
+router.post("/authentication/signup", signUpUser);
+router.post("/authentication/login", loginUser);
 
 export default router;
