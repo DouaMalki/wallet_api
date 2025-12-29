8d2ef9fe634aac7f084aedb97bf518efbc3b5650
JEANINE JABER <jj7222.49@gmail.com>
2025-12-29 22:25:03 +0200

    update

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -64,8 +64,8 @@ export async function getUsersWithHighTripCreation(req, res) {
 
     res.json(users);
   } catch (err) {
-    console.log("High trip creation users error", err);
-    res.status(500).json({ message: "Internal server error" });
+    console.log("High trip creation users error", err.message);
+    res.status(500).json({ message: err.message });
   }
 }
 
