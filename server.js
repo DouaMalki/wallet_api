92dc62a53b76a5510afe154460f9582b232ff944
shahdAbumoala <shahdabumoala@gmail.com>
2026-01-07 00:46:48 +0200

    Resolve merge conflict in server.js

--- server.js
+++ server.js
@@ -5,13 +5,11 @@ import rateLimiter from "./middleware/rateLimiter.js";
 import job from "./config/cron.js";
 // routers that will be used by the express app
 import reportsRouter from "./routers/reportsRouter.js";
-<<<<<<< HEAD
-import locationsRouter from "./routers/locationsRouter.js";
 
-=======
+import locationsRouter from "./routers/locationsRouter.js";
 import authenticationRouter from "./routers/authenticationRouter.js";
 import usersRouter from "./routers/usersRouter.js";
->>>>>>> 9bc77628560f50063f1561410aed4b306c5881f8
+
 
 const app = express();
 if (process.env.NODE_ENV === "production") job.start();
@@ -28,15 +26,12 @@ app.get("/api/health", (req, res) => {
 
 // routers used by the express app
 app.use("/api/reports", reportsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api", authenticationRouter);
 app.use("/api/locations", locationsRouter);
 
-<<<<<<< HEAD
 console.log("✅ server.js loaded - mounting /api/locations");
-=======
-app.use("/api/users", usersRouter);
 
-app.use("/api", authenticationRouter);
->>>>>>> 9bc77628560f50063f1561410aed4b306c5881f8
 
 
 // To test that the application connected to the database
