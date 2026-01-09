19ed4d63c87f169c77ff1e641f2417dfc718c3f6
DouaMalki <166130291+DouaMalki@users.noreply.github.com>
2026-01-09 21:47:30 +0200

    editProfile

--- server.js
+++ server.js
@@ -5,20 +5,15 @@ import rateLimiter from "./middleware/rateLimiter.js";
 import job from "./config/cron.js";
 // routers that will be used by the express app
 import reportsRouter from "./routers/reportsRouter.js";
-
-import locationsRouter from "./routers/locationsRouter.js";
 import authenticationRouter from "./routers/authenticationRouter.js";
-<<<<<<< HEAD
 import editProfileRouter from "./routers/editProfileRouter.js";
-=======
+
 import usersRouter from "./routers/usersRouter.js";
+import locationsRouter from "./routers/locationsRouter.js";
 import citiesRouter from "./routers/citiesRouter.js";
 import tripTypesRouter from "./routers/tripTypesRouter.js";
 import tripTypeRulesRouter from "./routers/tripTypeRulesRouter.js";
 import adminRouter from "./routers/adminRouter.js";
-
-
->>>>>>> d8955c4518b175922014ac0b2ca66cc5077aa655
 
 const app = express();
 if (process.env.NODE_ENV === "production") job.start();
@@ -36,11 +31,10 @@ app.get("/api/health", (req, res) => {
 
 // routers used by the express app
 app.use("/api/reports", reportsRouter);
-app.use("/api/users", usersRouter);
 app.use("/api", authenticationRouter);
-<<<<<<< HEAD
 app.use("/api", editProfileRouter);
-=======
+
+app.use("/api/users", usersRouter);
 app.use("/api/locations", locationsRouter);
 app.use("/api/cities", citiesRouter);
 app.use("/api/trip-types", tripTypesRouter);
@@ -52,8 +46,6 @@ app.use("/api/admin", adminRouter);
 
 console.log("✅ server.js loaded - mounting /api/locations");
 
-
->>>>>>> d8955c4518b175922014ac0b2ca66cc5077aa655
 
 // To test that the application connected to the database
 const PORT = process.env.PORT || 5000;
