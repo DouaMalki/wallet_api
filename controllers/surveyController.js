49ef3fa011a302dca4b8b09df3208cf1137634c9
DouaMalki <166130291+DouaMalki@users.noreply.github.com>
2026-01-19 13:30:41 +0200

    updating

--- controllers/surveyController.js
+++ controllers/surveyController.js
@@ -1,4 +1,3 @@
-// controllers/systemSettingsController.js
 import { sql } from "../config/db.js";
 
 export async function getProblemTypes(req, res) {
@@ -9,27 +8,27 @@ export async function getProblemTypes(req, res) {
       ORDER BY created_at DESC
       LIMIT 1
     `;
-
     if (result.length === 0) {
       return res.status(404).json({
         message: "System settings not found",
       });
     }
-
     const problemTypes = result[0].problem_types;
 
-    // Convert JSONB object → array
-    // { crash:0, other:0 } → [{id:"crash", label:"crash"}]
-    const problems = Object.keys(problemTypes).map((key) => ({
-      id: key,
-      label: key.replace(/_/g, " "),
+    // JSONB ARRAY
+    if (!Array.isArray(problemTypes)) {
+      return res.status(400).json({
+        message: "problem_types must be an array",
+      });
+    }
+    const problems = problemTypes.map((item) => ({
+      id: item,
+      label: item.replace(/_/g, " "),
     }));
-
-    res.json(problems);
-
+    return res.status(200).json(problems);
   } catch (err) {
     console.error("Get problem types error:", err);
-    res.status(500).json({
+    return res.status(500).json({
       message: "Failed to fetch problem types",
     });
   }
