07ebf19560cb61e863f9669bb0e52a4165479c7e
shahdAbumoala <shahdabumoala@gmail.com>
2026-03-07 11:24:04 +0200

    Modify seedCityService 14 + add refresh

--- routers/seedRouter.js
+++ routers/seedRouter.js
@@ -18,11 +18,12 @@
 // export default router;
 
 import express from "express";
-import { refreshSeed, ensureMissingSeed } from "../controllers/seedController.js";
+import { ensureSeed, refreshSeed, ensureMissingSeed } from "../controllers/seedController.js";
 
 const router = express.Router();
 
 router.get("/refresh", refreshSeed);
+router.get("/ensure", ensureSeed);
 router.get("/ensure-missing", ensureMissingSeed);
 
 console.log("✅ seedRouter loaded");
