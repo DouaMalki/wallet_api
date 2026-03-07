c8c6c09f4448a838317cd9f90d48737e9a3a4358
shahdAbumoala <shahdabumoala@gmail.com>
2026-03-07 10:48:38 +0200

    Modify seedCityService 13 + add refresh

--- routers/seedRouter.js
+++ routers/seedRouter.js
@@ -1,16 +1,28 @@
+// import express from "express";
+// import { refreshSeed, ensureSeed, ensureAllSeed, ensureMissingSeed } from "../controllers/seedController.js";
+
+// const router = express.Router();
+
+// //router.get("/refresh", refreshSeed);
+// //router.get("/ensure", ensureSeed);
+// //router.get("/ensure-all", ensureAllSeed);
+
+// // GET /api/seed/refresh?cityId=ramallah&tripType=cultural
+// router.get("/refresh", refreshSeed);
+
+// // GET /api/seed/ensure-missing?cityId=ramallah&tripType=cultural&maxTotal=40&batch=5&pause=2000
+// router.get("/ensure-missing", ensureMissingSeed);
+
+// console.log("✅ seedRouter loaded");
+
+// export default router;
+
 import express from "express";
-import { refreshSeed, ensureSeed, ensureAllSeed, ensureMissingSeed } from "../controllers/seedController.js";
+import { refreshSeed, ensureMissingSeed } from "../controllers/seedController.js";
 
 const router = express.Router();
 
-//router.get("/refresh", refreshSeed);
-//router.get("/ensure", ensureSeed);
-//router.get("/ensure-all", ensureAllSeed);
-
-// GET /api/seed/refresh?cityId=ramallah&tripType=cultural
 router.get("/refresh", refreshSeed);
-
-// GET /api/seed/ensure-missing?cityId=ramallah&tripType=cultural&maxTotal=40&batch=5&pause=2000
 router.get("/ensure-missing", ensureMissingSeed);
 
 console.log("✅ seedRouter loaded");
